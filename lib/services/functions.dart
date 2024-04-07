80a0655edda37ba9b496eba2bb1cde76475d95f0
igieske <igoza99@gmail.com>
2024-04-07 16:16:16 +0300

    Add functions.dart with url_launcher callers.

---
+++ lib/services/functions.dart
@@ -0,0 +1,24 @@
+// общие функции и константы
+
+import 'package:url_launcher/url_launcher.dart';
+
+
+// позвонить
+Future<void> makePhoneCall(String phoneNumber) async {
+  final Uri launchUri = Uri(scheme: 'tel', path: phoneNumber);
+  await launchUrl(launchUri);
+}
+
+// написать email
+Future<void> goEmail(String email) async {
+  final Uri launchUri = Uri(scheme: 'mailto', path: email);
+  await launchUrl(launchUri);
+}
+
+// перейти по url
+Future<void> goUrl(String url, {LaunchMode mode = LaunchMode.platformDefault}) async {
+  final launch = await launchUrl(Uri.parse(url), mode: mode);
+  if (!launch) {
+    throw Exception('Не удалось перейти по ссылке');
+  }
+}
