cb4f0963b8b9f12b96645502cd8dcd995da475f4
igieske <igoza99@gmail.com>
2024-04-07 16:19:44 +0300

    Set main.dart to use Scaffold and routing.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:baroo/layout/scaffold/scaffold_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'router.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,40 +13,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Baroo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+    final scaffoldBloc = ScaffoldBloc();
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('b'),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('hi there'),
-          ],
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => scaffoldBloc),
+      ],
+      child: MaterialApp.router(
+        title: 'Baroo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
         ),
+        routerConfig: appRouter,
       ),
     );
   }
