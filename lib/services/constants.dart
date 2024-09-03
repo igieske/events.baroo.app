82a6e7c18005beca994834ab23eb2025253fbb0d
igieske <igoza99@gmail.com>
2024-09-03 23:33:20 +0300

    Constants: remove caseTypes list.

--- lib/services/constants.dart
+++ lib/services/constants.dart
@@ -28,33 +28,6 @@ final List<PostType> postTypes = [
   ),
 ];
 
-final List<CaseType> caseTypes = [
-  CaseType(
-      id: 5,
-      type: CaseTypes.liveMusic,
-      slug: 'live-music',
-      label: 'Живая музыка'
-  ),
-  CaseType(
-      id: 6,
-      type: CaseTypes.poetry,
-      slug: 'poetry',
-      label: 'Поэзия'
-  ),
-  CaseType(
-      id: 7,
-      type: CaseTypes.standup,
-      slug: 'standup',
-      label: 'Стендап'
-  ),
-  CaseType(
-      id: 13,
-      type: CaseTypes.theatre,
-      slug: 'theatre',
-      label: 'Театр'
-  ),
-];
-
 final List<CaseGenre> caseGenres = [
   CaseGenre(
     type: CaseGenres.jazz,
