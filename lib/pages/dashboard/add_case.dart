4a8022e30e4f8e8105b375e212475f3e4515196f
igieske <igoza99@gmail.com>
2024-09-06 22:49:45 +0300

    AddCase: replace GridView by Wrap layout. Use Autocomplete for Bars select.

--- lib/pages/dashboard/add_case.dart
+++ lib/pages/dashboard/add_case.dart
@@ -1,4 +1,7 @@
+import 'package:baroo/models/bar.dart';
+import 'package:baroo/services/dict/dict_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 class AddCasePage extends StatefulWidget {
@@ -10,10 +13,18 @@ class AddCasePage extends StatefulWidget {
 
 class _AddCasePageState extends State<AddCasePage> {
 
+  late final DictCubit dict;
+
   GlobalKey addCaseFormKey = GlobalKey();
 
   final TextEditingController _barCtrl = TextEditingController();
-  final TextEditingController _usernameCtrl2 = TextEditingController();
+  final TextEditingController _dateCtrl = TextEditingController();
+
+  @override
+  void initState() {
+    dict = context.read<DictCubit>();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,47 +35,89 @@ class _AddCasePageState extends State<AddCasePage> {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
-        child: Form(
-          key: addCaseFormKey,
-          child: Align(
-            alignment: Alignment.topCenter,
-            child: ConstrainedBox(
-              constraints: const BoxConstraints(maxWidth: 840),
-              child: GridView(
-                shrinkWrap: true,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 16,
+        child: BlocBuilder<DictCubit, DictState>(
+          builder: (context, dict) {
+
+            if (dict is DictLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (dict is Dict) {
+
+              return Form(
+                key: addCaseFormKey,
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints(maxWidth: 840),
+                    child: LayoutBuilder(
+                      builder: (BuildContext context, BoxConstraints constraints) {
+
+                        return Wrap(
+                          spacing: 16,
+                          runSpacing: 16,
+                          children: [
+
+                            Autocomplete<Bar>(
+                              optionsBuilder: (TextEditingValue textEditingValue) {
+                                if (textEditingValue.text.length < 2) {
+                                  return const Iterable<Bar>.empty();
+                                }
+                                return dict.bars.where((bar) => bar.name.toLowerCase().contains(textEditingValue.text.toLowerCase()));
+                              },
+                              displayStringForOption: (Bar bar) => bar.name,
+                              onSelected: (Bar selection) {
+                                print('Выбрано: ${selection.name}, ID: ${selection.id}');
+                              },
+                              fieldViewBuilder: (BuildContext context, TextEditingController textEditingController, FocusNode focusNode, VoidCallback onFieldSubmitted) {
+                                return TextField(
+                                  controller: textEditingController,
+                                  focusNode: focusNode,
+                                  decoration: const InputDecoration(
+                                    hintText: 'Введите имя бара',
+                                  ),
+                                );
+                              },
+                            ),
+
+                            TextFormField(
+                              controller: _barCtrl,
+                              decoration: const InputDecoration(
+                                label: Text('Уточнение о месте проведения'),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.trim().isEmpty) {
+                                  return 'Please enter username or email';
+                                }
+                                return null;
+                              },
+                            ),
+
+                            TextFormField(
+                              controller: _dateCtrl,
+                              decoration: const InputDecoration(
+                                label: Text('username or email'),
+                              ),
+                              validator: (value) {
+                                if (value == null || value.trim().isEmpty) {
+                                  return 'Please enter username or email';
+                                }
+                                return null;
+                              },
+                            ),
+                          ],
+                        );
+
+                      },
+
+                    ),
+                  ),
                 ),
-                children: [
-                  TextFormField(
-                    controller: _barCtrl,
-                    decoration: const InputDecoration(
-                      label: Text('username or email'),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.trim().isEmpty) {
-                        return 'Please enter username or email';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    controller: _usernameCtrl2,
-                    decoration: const InputDecoration(
-                      label: Text('username or email'),
-                    ),
-                    validator: (value) {
-                      if (value == null || value.trim().isEmpty) {
-                        return 'Please enter username or email';
-                      }
-                      return null;
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
+              );
+
+            } else {
+              return const Center(child: Text('ошибка'));
+            }
+
+          },
         ),
       ),
     );
