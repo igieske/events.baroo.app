4463b36a0d78b0d201cbbbe791179456fdd64a44
igieske <igoza99@gmail.com>
2024-09-07 00:30:15 +0300

    AddCasePage: implement response layout.

--- lib/pages/dashboard/add_case.dart
+++ lib/pages/dashboard/add_case.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
 import 'package:baroo/models/bar.dart';
 import 'package:baroo/services/dict/dict_cubit.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 class AddCasePage extends StatefulWidget {
@@ -33,92 +36,122 @@ class _AddCasePageState extends State<AddCasePage> {
         centerTitle: true,
         title: const Text('Добавить событие'),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16),
-        child: BlocBuilder<DictCubit, DictState>(
-          builder: (context, dict) {
+      body: BlocBuilder<DictCubit, DictState>(
+        builder: (context, dict) {
 
-            if (dict is DictLoading) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (dict is Dict) {
+          if (dict is DictLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (dict is Dict) {
 
-              return Form(
-                key: addCaseFormKey,
-                child: Align(
-                  alignment: Alignment.topCenter,
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(maxWidth: 840),
-                    child: LayoutBuilder(
-                      builder: (BuildContext context, BoxConstraints constraints) {
+            return Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                color: Colors.white,
+                constraints: const BoxConstraints(maxWidth: 840),
+                padding: ResponsiveBreakpoints.of(context).smallerOrEqualTo(TABLET)
+                    ? const EdgeInsets.symmetric(horizontal: 16) : null,
+                child: Form(
+                  key: addCaseFormKey,
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      return Wrap(
+                        spacing: 16,
+                        runSpacing: 16,
+                        children: [
 
-                        return Wrap(
-                          spacing: 16,
-                          runSpacing: 16,
-                          children: [
-
-                            Autocomplete<Bar>(
-                              optionsBuilder: (TextEditingValue textEditingValue) {
-                                if (textEditingValue.text.length < 2) {
-                                  return const Iterable<Bar>.empty();
-                                }
-                                return dict.bars.where((bar) => bar.name.toLowerCase().contains(textEditingValue.text.toLowerCase()));
-                              },
-                              displayStringForOption: (Bar bar) => bar.name,
-                              onSelected: (Bar selection) {
-                                print('Выбрано: ${selection.name}, ID: ${selection.id}');
-                              },
-                              fieldViewBuilder: (BuildContext context, TextEditingController textEditingController, FocusNode focusNode, VoidCallback onFieldSubmitted) {
-                                return TextField(
-                                  controller: textEditingController,
-                                  focusNode: focusNode,
-                                  decoration: const InputDecoration(
-                                    hintText: 'Введите имя бара',
+                          ResponsiveRowColumnItem(
+                            child: SizedBox(
+                              width: ResponsiveValue(
+                                context,
+                                defaultValue: constraints.maxHeight,
+                                conditionalValues: [
+                                  Condition.equals(
+                                    name: MOBILE,
+                                    value: constraints.maxHeight,
                                   ),
-                                );
-                              },
-                            ),
-
-                            TextFormField(
-                              controller: _barCtrl,
-                              decoration: const InputDecoration(
-                                label: Text('Уточнение о месте проведения'),
+                                  Condition.equals(
+                                    name: TABLET,
+                                    value: constraints.maxHeight / 2,
+                                  ),
+                                  Condition.largerThan(
+                                    name: TABLET,
+                                    value: constraints.maxHeight / 3,
+                                  ),
+                                ],
+                              ).value,
+                              child: Autocomplete<Bar>(
+                                optionsBuilder: (TextEditingValue textEditingValue) {
+                                  if (textEditingValue.text.length < 2) {
+                                    return const Iterable<Bar>.empty();
+                                  }
+                                  return dict.bars.where((bar) => bar.name.toLowerCase().contains(textEditingValue.text.toLowerCase()));
+                                },
+                                displayStringForOption: (Bar bar) => bar.name,
+                                onSelected: (Bar selection) {
+                                  print('Выбрано: ${selection.name}, ID: ${selection.id}');
+                                },
+                                fieldViewBuilder: (BuildContext context, TextEditingController textEditingController, FocusNode focusNode, VoidCallback onFieldSubmitted) {
+                                  return TextField(
+                                    controller: textEditingController,
+                                    focusNode: focusNode,
+                                    decoration: const InputDecoration(
+                                      hintText: 'Введите имя бара',
+                                    ),
+                                  );
+                                },
                               ),
-                              validator: (value) {
-                                if (value == null || value.trim().isEmpty) {
-                                  return 'Please enter username or email';
-                                }
-                                return null;
-                              },
                             ),
+                          ),
 
-                            TextFormField(
-                              controller: _dateCtrl,
-                              decoration: const InputDecoration(
-                                label: Text('username or email'),
+                          ResponsiveRowColumnItem(
+                            child: SizedBox(
+                              width: ResponsiveValue(
+                                context,
+                                defaultValue: constraints.maxHeight,
+                                conditionalValues: [
+                                  Condition.equals(
+                                    name: MOBILE,
+                                    value: constraints.maxHeight,
+                                  ),
+                                  Condition.equals(
+                                    name: TABLET,
+                                    value: constraints.maxHeight / 2,
+                                  ),
+                                  Condition.largerThan(
+                                    name: TABLET,
+                                    value: constraints.maxHeight / 3,
+                                  ),
+                                ],
+                              ).value,
+                              child: TextFormField(
+                                controller: _barCtrl,
+                                decoration: const InputDecoration(
+                                  label: Text('Уточнение о месте проведения'),
+                                ),
+                                validator: (value) {
+                                  if (value == null || value.trim().isEmpty) {
+                                    return 'Please enter username or email';
+                                  }
+                                  return null;
+                                },
                               ),
-                              validator: (value) {
-                                if (value == null || value.trim().isEmpty) {
-                                  return 'Please enter username or email';
-                                }
-                                return null;
-                              },
                             ),
-                          ],
-                        );
+                          ),
 
-                      },
 
-                    ),
+                        ],
+                      );
+                    },
                   ),
                 ),
-              );
+              ),
+            );
 
-            } else {
-              return const Center(child: Text('ошибка'));
-            }
+          } else {
+            return const Center(child: Text('ошибка'));
+          }
 
-          },
-        ),
+        },
       ),
     );
   }
