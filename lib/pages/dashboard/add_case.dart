9e8fd2ead4f82794a40463fd6fec2f9f5e341815
igieske <igoza99@gmail.com>
2024-10-29 22:17:01 +0300

    AddCasePage: add artists autocomplete select.

--- lib/pages/dashboard/add_case.dart
+++ lib/pages/dashboard/add_case.dart
@@ -11,6 +11,7 @@ import 'package:collection/collection.dart';
 
 import 'package:baroo/layout/bric.dart';
 import 'package:baroo/layout/chiper.dart';
+import 'package:baroo/models/artist.dart';
 import 'package:baroo/models/case_type.dart';
 import 'package:baroo/models/bar.dart';
 import 'package:baroo/services/dict/dict_cubit.dart';
@@ -656,28 +657,111 @@ class _AddCasePageState extends State<AddCasePage> with TickerProviderStateMixin
                   [
                     const AddCasePageTitle(title: 'Артисты и жанры'),
 
-                    // жанры
-                    Bric(
-                      child: Chiper(
-                        labelText: 'Жанры',
-                        options: dict.genres.map((genre) {
-                          return ChiperOption(
-                              value: genre.slug,
-                              label: genre.label,
-                              subOptions: genre.subgenres?.map((subgenre) {
-                                return ChiperOption(
-                                  value: subgenre.slug,
-                                  label: subgenre.label,
+                    Brics(
+                      gap: 16,
+                      crossGap: 16,
+                      children: [
+
+                        // жанры
+                        Bric(
+                          child: Chiper(
+                            labelText: 'Жанры',
+                            options: dict.genres.map((genre) {
+                              return ChiperOption(
+                                  value: genre.slug,
+                                  label: genre.label,
+                                  subOptions: genre.subgenres?.map((subgenre) {
+                                    return ChiperOption(
+                                      value: subgenre.slug,
+                                      label: subgenre.label,
+                                    );
+                                  }).toList()
+                              );
+                            }).toList(),
+                            values: genresValue,
+                            onChanged: (values) {
+                              setState(() => genresValue = values);
+                            },
+                          ),
+                        ),
+
+                        // артисты
+                        Bric(
+                          size: const {
+                            BrickWidth.sm: 6,
+                          },
+                          builder: (context, bricWidth) {
+                            return Autocomplete<Artist>(
+                              displayStringForOption: (Artist artist) => artist.name,
+                              optionsBuilder: (TextEditingValue textEditingValue) {
+                                if (textEditingValue.text.length < 2) {
+                                  return const Iterable<Artist>.empty();
+                                }
+                                return dict.artists.where((Artist artist) => artist.name.toLowerCase()
+                                    .contains(textEditingValue.text.toLowerCase()));
+                              },
+                              optionsViewBuilder: (BuildContext context,
+                                  AutocompleteOnSelected<Artist> onSelected,
+                                  Iterable<Artist> options) {
+                                return Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Material(
+                                    elevation: 4.0,
+                                    child: ConstrainedBox(
+                                      constraints: BoxConstraints(maxWidth: bricWidth),
+                                      child: ListView.builder(
+                                        padding: EdgeInsets.zero,
+                                        shrinkWrap: true,
+                                        itemCount: options.length,
+                                        itemBuilder: (BuildContext context, int index) {
+                                          final Artist artist = options.elementAt(index);
+                                          return InkWell(
+                                            onTap: () => onSelected(artist),
+                                            child: Builder(
+                                                builder: (BuildContext context) {
+                                                  final bool highlight = AutocompleteHighlightedOption.of(context) == index;
+                                                  if (highlight) {
+                                                    SchedulerBinding.instance.addPostFrameCallback((Duration timeStamp) {
+                                                      Scrollable.ensureVisible(context, alignment: 0.5);
+                                                    });
+                                                  }
+                                                  return Container(
+                                                    color: highlight ? Theme.of(context).focusColor : null,
+                                                    padding: const EdgeInsets.all(16.0),
+                                                    child: Text(
+                                                      RawAutocomplete.defaultStringForOption(artist.name),
+                                                    ),
+                                                  );
+                                                }
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    ),
+                                  ),
                                 );
-                              }).toList()
-                          );
-                        }).toList(),
-                        values: genresValue,
-                        onChanged: (values) {
-                          setState(() => genresValue = values);
-                        },
-                      ),
+                              },
+                              fieldViewBuilder: (context, controller, focusNode, onFieldSubmitted) {
+                                return TextField(
+                                  controller: controller,
+                                  focusNode: focusNode,
+                                  decoration: const InputDecoration(
+                                    hintText: 'Артисты',
+                                  ),
+                                  onSubmitted: (value) => onFieldSubmitted(),
+                                );
+                              },
+                              onSelected: (Artist selection) {
+                                print('Выбрано: ${selection.name}, ID: ${selection.id}');
+                              },
+
+                            );
+                          },
+                        ),
+
+                      ],
                     ),
+
                   ],
 
                 ].map((tabBarChildren) => SingleChildScrollView(
