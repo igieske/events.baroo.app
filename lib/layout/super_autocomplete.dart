e3f175bb378fb974c5e7557a5582d6d10a2bda0e
igieske <igoza99@gmail.com>
2024-11-01 00:26:54 +0300

    SuperAutocomplete: dev iteration commit.

--- lib/layout/super_autocomplete.dart
+++ lib/layout/super_autocomplete.dart
@@ -2,31 +2,66 @@ import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
 
-class SuperAutocomplete<T extends Object> extends StatelessWidget {
+class SuperAutocomplete<T extends Object> extends StatefulWidget {
+  final TextEditingController? controller;
   final List<T> options;
+  final T? initialValue;
   final double maxWidth;
   final String hintText;
   final String Function(T) displayStringForOption;
   final void Function(T) onSelected;
+  final void Function() onCleared;
 
   const SuperAutocomplete({
     super.key,
+    this.controller,
     required this.options,
+    this.initialValue,
     required this.maxWidth,
     required this.hintText,
     required this.displayStringForOption,
     required this.onSelected,
+    required this.onCleared,
   });
+
+  @override
+  State<SuperAutocomplete<T>> createState() => _SuperAutocompleteState<T>();
+}
+
+class _SuperAutocompleteState<T extends Object> extends State<SuperAutocomplete<T>> {
+  late TextEditingController _controller;
+  late bool _isSelected;
+
+  void _clear(TextEditingController? controller) {
+    (controller ?? _controller).clear();
+    setState(() => _isSelected = false);
+    widget.onCleared();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? TextEditingController();
+    _isSelected = widget.initialValue != null;
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Autocomplete<T>(
-      displayStringForOption: displayStringForOption,
+      // todo
+      // initialValue: ,
+      displayStringForOption: widget.displayStringForOption,
       optionsBuilder: (TextEditingValue textEditingValue) {
         if (textEditingValue.text.length < 2) {
           return Iterable<T>.empty();
         }
-        return options.where((T option) => displayStringForOption(option)
+        return widget.options.where((T option) => widget.displayStringForOption(option)
             .toLowerCase()
             .contains(textEditingValue.text.toLowerCase()));
       },
@@ -36,9 +71,9 @@ class SuperAutocomplete<T extends Object> extends StatelessWidget {
         return Align(
           alignment: Alignment.topLeft,
           child: Material(
-            elevation: 4.0,
+            elevation: 4,
             child: ConstrainedBox(
-              constraints: BoxConstraints(maxWidth: maxWidth),
+              constraints: BoxConstraints(maxWidth: widget.maxWidth),
               child: ListView.builder(
                 padding: EdgeInsets.zero,
                 shrinkWrap: true,
@@ -46,9 +81,17 @@ class SuperAutocomplete<T extends Object> extends StatelessWidget {
                 itemBuilder: (BuildContext context, int index) {
                   final T option = options.elementAt(index);
                   return InkWell(
-                    onTap: () => onSelected(option),
+                    onTap: () {
+                      print('onTap!');
+                      // _controller.text = widget.displayStringForOption(option);
+
+                      // передаём в onSelected
+                      onSelected(option);
+
+                    },
                     child: Builder(
                       builder: (BuildContext context) {
+                        // todo: hover цвет не делать для мобилы
                         final bool highlight = AutocompleteHighlightedOption.of(context) == index;
                         if (highlight) {
                           SchedulerBinding.instance.addPostFrameCallback((Duration timeStamp) {
@@ -59,7 +102,7 @@ class SuperAutocomplete<T extends Object> extends StatelessWidget {
                           color: highlight ? Theme.of(context).focusColor : null,
                           padding: const EdgeInsets.all(16),
                           child: Text(
-                            RawAutocomplete.defaultStringForOption(displayStringForOption(option)),
+                            RawAutocomplete.defaultStringForOption(widget.displayStringForOption(option)),
                           ),
                         );
                       },
@@ -75,11 +118,51 @@ class SuperAutocomplete<T extends Object> extends StatelessWidget {
         return TextField(
           controller: controller,
           focusNode: focusNode,
-          decoration: InputDecoration(hintText: hintText),
-          onSubmitted: (value) => onFieldSubmitted(),
+          decoration: InputDecoration(
+            labelText: widget.hintText,
+            prefix: _isSelected ? Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.surfaceContainer,
+                borderRadius: BorderRadius.circular(4),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 1),
+              child: Text(controller.text),
+            ) : null,
+            suffixIcon: _isSelected
+              ? IconButton(
+                icon: const Icon(Icons.clear),
+                onPressed: () => _clear(controller),
+              )
+              : null,
+          ),
+          readOnly: _isSelected,
+          onSubmitted: (value) {
+            print('onSubmitted: $value');
+
+            // if (!widget.options.any((T option) => widget.displayStringForOption(option) == value)) {
+            //   // controller.clear();
+            //   widget.onCleared();
+            // }
+
+            // при нажатии Enter – происходит сабмит
+            // если опция выбрана - кидает в onSelect
+            onFieldSubmitted();
+          },
         );
       },
-      onSelected: onSelected,
+      onSelected: (T selectedValue) {
+
+
+        _controller.clear();
+        _controller.text = '';
+
+
+        _controller.value = const TextEditingValue(text: '');
+        print('onSelected! ${selectedValue.toString()}');
+        widget.onSelected(selectedValue);
+
+        setState(() => _isSelected = true);
+      },
     );
   }
 }
