1c3f9dc5dcdcd91d5e3d6429131a8ae650bca0ce
igieske <igoza99@gmail.com>
2025-01-03 00:31:42 +0530

    Http service refactoring.

--- lib/services/http.dart
+++ lib/services/http.dart
@@ -1,57 +1,41 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class ApiParams {
-  String action;
-  Object? body;
-
-  ApiParams(this.action, [this.body]);
-}
 
 class Http {
 
-  static Future post(ApiParams params) async {
-    print('----> ${params.action}');
-
-    final request = http.Request('POST',
-        Uri.parse('https://baroo.ru/wp-content/themes/baroo-child/app-handler.php')
-    );
-    request.body = json.encode(params.body);
-    request.headers.addAll({
-      'Content-Type': 'application/json',
-      'Action': params.action,
-    });
+  static Future post(String method, [Map<String, dynamic>? body]) async {
+    print('----> $method');
+    body ??= {};
 
     try {
-      http.StreamedResponse response = await request.send();
 
-      if (response.statusCode == 200) {
-        final responseString = await response.stream.bytesToString();
-        // парсим json
-        Map<String, dynamic> decodedJSON;
-        try {
-          decodedJSON = json.decode(responseString) as Map<String, dynamic>;
-        } on FormatException catch (e) {
-          // ! ошибка при парсинге JSON
-          print('! Ответ не JSON: ${e.message}');
-          decodedJSON = jsonDecode('{"Error": 3, "Message": "Ошибка запроса, попробуйте позже"}');
-        }
-        // ответ
-        print(decodedJSON);
-        return decodedJSON;
-      } else {
-        // ! ошибка при ожидании потока респонса
-        print('! ошибка при ожидании потока респонса');
-        return jsonDecode('{"Error": 3, "Message": "Ошибка запроса, попробуйте позже."}');
+      // запрос
+      final response = await http.post(
+        Uri.https('baroo.ru', 'wp-content/themes/baroo-child/app/$method.php'),
+        body: json.encode(body),
+      );
+
+      // print(response.body);
+
+      // todo: проверять response.statusCode
+      // print(response.statusCode);
+
+      // парсим json
+      Map<String, dynamic> decodedJson;
+      try {
+        decodedJson = json.decode(response.body) as Map<String, dynamic>;
+      } on FormatException catch (e) {
+        throw('! Ответ не JSON: ${e.message}');
       }
 
-    } catch (e) {
-      // ! ошибка при отправке запроса
-      print('! ошибка при отправке запроса');
-      print(e);
-      return jsonDecode('{"Error": 3, "Message": "Ошибка. Проверьте подключение к интернету."}');
-    }
+      return decodedJson['data'] ?? {};
 
+    } catch (e) {
+      // todo
+      print('! ошибка при отправке запроса: $e');
+    }
+    return null;
   }
 
 }
