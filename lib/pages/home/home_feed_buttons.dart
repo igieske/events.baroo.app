a9fadf002a48a2ab61ce685aa475b7819c41cb29
igieske <igoza99@gmail.com>
2025-01-03 12:30:38 +0530

    HomeFeedButtons: update layout and data passing.

--- lib/pages/home/home_feed_buttons.dart
+++ lib/pages/home/home_feed_buttons.dart
@@ -2,40 +2,69 @@ import 'package:flutter/material.dart';
 
 
 class HomeFeedButtons extends StatelessWidget {
+  final int casesCount;
+  final int pastCasesCount;
   final VoidCallback onOpenPastCases;
 
-  const HomeFeedButtons({super.key, required this.onOpenPastCases});
+  const HomeFeedButtons({
+    super.key,
+    required this.casesCount,
+    required this.pastCasesCount,
+    required this.onOpenPastCases,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      spacing: 10,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
 
         Row(
           spacing: 6,
           children: [
 
-            Expanded(
-              child: FilledButton.icon(
-                onPressed: onOpenPastCases,
-                icon: Icon(Icons.chevron_left),
-                label: Text('Прошли'),
+            FilledButton.tonal(
+              onPressed: onOpenPastCases,
+              style: FilledButton.styleFrom(
+                minimumSize: Size(50, 50),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.chevron_left),
+                  Icon(Icons.history),
+                  const SizedBox(width: 5),
+                  Text(pastCasesCount.toString()),
+                  const SizedBox(width: 5),
+                ],
               ),
             ),
 
             Expanded(
               child: FilledButton.tonal(
                 onPressed: null,
-                child: Text('Всего: 5')
+                style: FilledButton.styleFrom(
+                  disabledForegroundColor: Color(0xFF475C73),
+                ),
+                child: Text('Найдено: $casesCount'),
               ),
             ),
 
-            Expanded(
-              child: FilledButton.icon(
-                onPressed: () {},
-                icon: Icon(Icons.chevron_right),
-                iconAlignment: IconAlignment.end,
-                label: Text('Скрыто', maxLines: 1),
+            FilledButton.tonal(
+              onPressed: () {},
+              style: FilledButton.styleFrom(
+                minimumSize: Size(50, 50),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(width: 5),
+                  Text('X'),
+                  const SizedBox(width: 5),
+                  Icon(Icons.visibility_off_outlined),
+                  Icon(Icons.chevron_right),
+                ],
               ),
             ),
 
