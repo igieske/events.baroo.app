f97210012e6135c5a2e38503a3f8cef9b5178692
igieske <igoza99@gmail.com>
2025-01-03 13:55:19 +0530

    Avatar: add averageColor property.

--- lib/models/avatar.dart
+++ lib/models/avatar.dart
@@ -1,14 +1,18 @@
+import 'dart:ui';
+
 class Avatar {
   final String thumbnailUrl;
   final String url;
   final int width;
   final int height;
+  final Color averageColor;
 
   Avatar({
     required this.thumbnailUrl,
     required this.url,
     required this.width,
     required this.height,
+    required this.averageColor,
   });
 
   double get aspectRatio => width / height;
@@ -19,15 +23,7 @@ class Avatar {
       url: json['url'] as String,
       width: json['width'] as int,
       height: json['height'] as int,
+      averageColor: Color(int.parse(json['averageColor'].replaceFirst('#', '0xff'))),
     );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'thumbnailUrl': thumbnailUrl,
-      'url': url,
-      'width': width,
-      'height': height,
-    };
   }
 }
